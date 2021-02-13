d03e2a2c5ce0d663e8d19e89a810bc8c5876ce33
FBSeletronica <fs.embarcados@gmail.com>
2021-02-13 18:59:53 -0300

    Deploy website - based on 7d321dad321d582a44dfc160efbf4bf81134e839

---
+++ 1b490027.e11417ff.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{72:function(o){o.exports=JSON.parse('{"permalink":"/docs-franzininho-site/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
