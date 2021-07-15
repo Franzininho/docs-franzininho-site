700e4c59185c6f16203e09c860df76129c51179f
Fabio <fs.embarcados@gmail.com>
2021-07-14 21:36:48 -0300

    Deploy website - based on 2fd785200b4d15245210355dc4d810cb30ba6486

---
+++ 63a86bbc.cb6523f2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{84:function(o){o.exports=JSON.parse('{"allTagsPath":"/docs-franzininho-site/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/docs-franzininho-site/blog/tags/hello"}')}}]);
---
+++ en/657be55d.576093a1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{87:function(s){s.exports=JSON.parse('{"allTagsPath":"/docs-franzininho-site/en/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/docs-franzininho-site/en/blog/tags/docusaurus"}')}}]);
