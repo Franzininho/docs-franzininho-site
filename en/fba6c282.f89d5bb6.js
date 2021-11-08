923cd333733b8eff3a058b5529bc390b52d1691a
Fabio <fs.embarcados@gmail.com>
2021-11-08 19:37:37 -0300

    Deploy website - based on 99708138d4f640785b682a200df7dd79f1dd46a9

--- en/fba6c282.f89d5bb6.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[79],{149:function(o){o.exports=JSON.parse('{"permalink":"/en/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
