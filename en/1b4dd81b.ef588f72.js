949589f51662c5eaf786fd142e9532944ac3698e
Fabio <fs.embarcados@gmail.com>
2021-11-08 19:32:46 -0300

    Deploy website - based on 99708138d4f640785b682a200df7dd79f1dd46a9

--- en/1b4dd81b.ef588f72.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{75:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/docs-franzininho-site/en/blog/welcome"},{"title":"Hello","permalink":"/docs-franzininho-site/en/blog/hello-world"},{"title":"Hola","permalink":"/docs-franzininho-site/en/blog/hola"}]}')}}]);
---
+++ en/a1db8d77.468e1134.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[44],{115:function(l){l.exports=JSON.parse('{"allTagsPath":"/en/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/en/blog/tags/hello"}')}}]);
