0fc1ffd916b913355538ff64d40e81d6a053767d
Fabio <fs.embarcados@gmail.com>
2021-11-08 20:17:59 -0300

    Deploy website - based on 99708138d4f640785b682a200df7dd79f1dd46a9

--- 1b490027.1c2921eb.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{74:function(o){o.exports=JSON.parse('{"permalink":"/docs-franzininho-site/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ en/62ad6a78.94b0c558.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{96:function(o){o.exports=JSON.parse('{"allTagsPath":"/en/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/en/blog/tags/facebook"}')}}]);
