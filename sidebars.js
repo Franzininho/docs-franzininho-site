c4a2816215fa544e475fc64e0d9ae0e5b7b488b2
dianasantos-ip <dianaipsantos@hotmail.com>
2022-05-09 16:54:34 -0300

    adicionado sidebars pwm

--- sidebars.js
+++ sidebars.js
@@ -125,6 +125,7 @@ module.exports = {
             'FranzininhoWifi/exemplos-circuitpython/circuitpython-display-oled-i2c',
             'FranzininhoWifi/exemplos-circuitpython/circuitpython-dht11',
             'FranzininhoWifi/exemplos-circuitpython/circuitpython-sensor-hcsr04'
+            'FranzininhoWifi/exemplos-circuitpython/pwm'
           ],
           collapsed: true
         },
