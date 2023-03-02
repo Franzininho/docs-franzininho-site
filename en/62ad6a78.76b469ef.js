89a12a911e023a3e62a3618cf4adfb1b490b15e3
Fábio Souza <fs.embarcados@gmail.com>
2023-03-02 16:52:31 -0300

    Deploy website - based on 1cf887298dee2a0de90393ab66c5ce82d2c764f1

---
+++ en/62ad6a78.76b469ef.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{99:function(o){o.exports=JSON.parse('{"allTagsPath":"/en/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/en/blog/tags/facebook"}')}}]);
--- en/f3f8ac04.6c5980ed.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[83],{153:function(s){s.exports=JSON.parse('{"allTagsPath":"/en/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/en/blog/tags/docusaurus"}')}}]);
